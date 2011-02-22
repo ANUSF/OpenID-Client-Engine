395c99eac9b3e4039d8a777f68a64cc8dc052f42
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-02-22 17:10:08 +1100

    Made the migration generator work.

--- lib/generators/openid_client/openid_client_generator.rb
+++ lib/generators/openid_client/openid_client_generator.rb
@@ -1,12 +1,19 @@
 require 'rails/generators'
 require 'rails/generators/migration'
 
-class AuthrGenerator < Rails::Generators::Base
+class OpenidClientGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
 
-  source_root File.expand_path("../templates", __FILE__)
+   # http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
+  def self.next_migration_number(dirname) #:nodoc:
+    if ActiveRecord::Base.timestamped_migrations
+      Time.now.utc.strftime("%Y%m%d%H%M%S")
+    else
+      "%.3d" % (current_migration_number(dirname) + 1)
+    end
+  end
 
   def create_migration_file
-    migration_template 'migration.rb', 'db/migrate/create_openid_users_table.rb'
+    migration_template '../../templates/migration.rb', 'db/migrate/create_openid_users_table.rb'
   end
 end
