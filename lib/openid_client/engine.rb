75b2a397aab42b0c810b2cf879e20faf9e47b27e
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-02-22 15:03:36 +1100

    Added an engine class.

---
+++ lib/openid_client/engine.rb
@@ -0,0 +1,18 @@
+require "openid_client"
+require "rails"
+
+module OpenidClient
+  class Config
+    class << self
+      # --- To add configuration options, we can do something like this:
+
+      # attr_accessor :server_url, :fallback_layer
+
+      # server_url = "http://vmap0.tiles.osgeo.org/wms/vmap0"
+      # fallback_layer = 'basic'
+    end
+  end
+
+  class Engine < Rails::Engine
+  end
+end
