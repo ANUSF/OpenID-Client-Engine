e9b66182bffcadc81363bbb633c3f6ac72555cb6
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-03-11 12:33:30 +1100

    Made default login and logout URLs configurable.

--- app/controllers/openid_client/sessions_controller.rb
+++ app/controllers/openid_client/sessions_controller.rb
@@ -1,15 +1,26 @@
 class OpenidClient::SessionsController < Devise::SessionsController
-  LOGIN_URL  = 'http://myopenid.com'
-  LOGOUT_URL = nil
+  helper_method :default_login, :default_logout, :server_human_name
+
+  def default_login
+    OpenidClient::Config.default_login
+  end
+
+  def default_logout
+    OpenidClient::Config.default_logout
+  end
+
+  def server_human_name
+    OpenidClient::Config.server_human_name || default_login
+  end
 
   def create
     login = params[resource_name][:identity_url]
 
-    if bypass_openid
+    if bypass_openid?
       resource_class = resource_name.to_s.classify.constantize
       resource = resource_class.find_or_create_by_identity_url(login)
     else
-      params[resource_name][:identity_url] = LOGIN_URL if login.blank?
+      params[resource_name][:identity_url] = default_login if login.blank?
       resource = warden.authenticate!(:scope => resource_name, :recall => "new")
     end
 
@@ -21,18 +32,17 @@ class OpenidClient::SessionsController < Devise::SessionsController
     set_flash_message :notice, :signed_out if signed_in?(resource_name)
     sign_out(resource_name)
 
-    if bypass_openid or params[:on_server].blank?
+    if bypass_openid? or params[:on_server].blank?
       redirect_to root_url
-    elsif LOGOUT_URL
+    elsif default_logout
       back = URI.escape(root_url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-      redirect_to "#{LOGOUT_URL}?return_url=#{back}"
+      redirect_to "#{default_logout}?return_url=#{back}"
     end
   end
 
   private
-
   # Whether to bypass OpenID verification.
-  def bypass_openid
+  def bypass_openid?
     [ 'test', 'cucumber' ].include?(Rails.env)
   end
 end
--- lib/openid_client/engine.rb
+++ lib/openid_client/engine.rb
@@ -4,18 +4,17 @@ require "rails"
 module OpenidClient
   class Config
     class << self
-      # --- To add configuration options, we can do something like this:
-
-      # attr_accessor :server_url, :fallback_layer
-
-      # server_url = "http://vmap0.tiles.osgeo.org/wms/vmap0"
-      # fallback_layer = 'basic'
+      attr_accessor :default_login, :default_logout, :server_human_name
     end
   end
 
   class Engine < Rails::Engine
     initializer "openid_client.add_middleware" do |app|
       app.middleware.insert_before(Warden::Manager, Rack::OpenID)
+    end
+
+    initializer "openid_client.configure" do |app|
+      OpenidClient::Config.default_login = 'http://myopenid.com'
     end
   end
 end
