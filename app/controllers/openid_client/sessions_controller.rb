7a206ca60f7912065bbfeaa71b0804d1a3d06591
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-04-29 12:25:08 +1000

    Only the exclamaion mark variant of warden.authenticate seems to work correctly.

--- app/controllers/openid_client/sessions_controller.rb
+++ app/controllers/openid_client/sessions_controller.rb
@@ -13,8 +13,8 @@ class OpenidClient::SessionsController < Devise::SessionsController
       resource = resource_class.find_or_create_by_identity_url(login)
     else
       params[resource_name][:identity_url] = default_login if login.blank?
-      resource = warden.authenticate(:scope => resource_name,
-                                     :recall => "#{controller_path}#new")
+      resource = warden.authenticate!(:scope => resource_name,
+                                      :recall => "#{controller_path}#new")
     end
 
     set_flash_message :notice, :signed_in
