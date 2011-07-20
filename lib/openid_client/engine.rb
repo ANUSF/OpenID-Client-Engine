301351b146b21195fa88a508b2a8000099db85f3
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-07-20 18:18:22 +1000

    Started on code that resubmits a non-get request after re-authentication.

--- lib/openid_client/engine.rb
+++ lib/openid_client/engine.rb
@@ -1,5 +1,28 @@
-require "openid_client"
-require "rails"
+require 'openid_client'
+require 'rails'
+require 'rack'
+
+
+module Rack
+  class MethodOverrideExtended < Rack::MethodOverride
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      req = Request.new(env)
+      method = req.params[METHOD_OVERRIDE_PARAM_KEY] ||
+        env[HTTP_METHOD_OVERRIDE_HEADER]
+      method = method.to_s.upcase
+      if HTTP_METHODS.include?(method)
+        env['rack.methodoverride.original_method'] = env['REQUEST_METHOD']
+        env['REQUEST_METHOD'] = method
+      end
+
+      @app.call(env)
+    end
+  end
+end
 
 module OpenidClient
   class Config
@@ -20,8 +43,9 @@ module OpenidClient
   end
 
   class Engine < Rails::Engine
-    initializer "openid_client.add_middleware" do |app|
-      app.middleware.insert_before(Warden::Manager, Rack::OpenID)
+    initializer 'openid_client.add_middleware' do |app|
+      app.middleware.insert_before Warden::Manager, Rack::OpenID
+      app.middleware.swap Rack::MethodOverride, Rack::MethodOverrideExtended
     end
   end
 end
--- lib/openid_client/helpers.rb
+++ lib/openid_client/helpers.rb
@@ -8,26 +8,55 @@ module OpenidClient
     # otherwise. This would typically be used as a before filter.
     def update_authentication
       timestamp = get_timestamp
-      Rails.logger.info "OID check: server timestamp = #{timestamp}"
+      info "server timestamp = #{timestamp}"
 
       state = load_oid_state
-      Rails.logger.info "OID check: initial state = #{state.inspect}"
+      info "initial state = #{state.inspect}"
+      info "csrf token = #{form_authenticity_token}"
 
       if not session[:openid_checked].blank?
-        Rails.logger.info "OID check: finished (redirecting to requested page)"
-        save_oid_state 'request_url' => nil, 'finished' => timestamp
+        info "finished re-authentication"
+        save_oid_state 'finished' => timestamp
         session[:openid_checked] = nil
-        target = state['request_url']
-      elsif state['finished'] != timestamp and
-          not request.path =~ /^#{new_user_session_path}\??/
-        Rails.logger.info "OID check: changes on server, re-authenticating"
-        save_oid_state 'request_url' => request.url, 'finished' => nil
+
+        target = state['request_target']
+        if target.nil?
+          info "no redirection required"
+        elsif target['_method'].nil?
+          info "redirecting to requested page"
+        else
+          info "resubmitting request"
+          if state['user_key'] != session['warden.user.user.key']
+            info "user key has changed from #{state['user_key']} " +
+              "to #{session['warden.user.user.key']}"
+          elsif not state['verified']
+            info "request did not pass csrf validation"
+          else
+            info "updating authenticity token"
+            target = target.merge({ request_forgery_protection_token =>
+                                    form_authenticity_token })
+            #request.headers['X-CSRF-Token'] = form_authenticity_token
+          end
+        end
+      elsif recheck_needed(timestamp, state)
+        info "starting re-authentication"
+        save_oid_state('request_target' => target_hash,
+                       'user_key' => session['warden.user.user.key'],
+                       'verified' => verified_request?,
+                       'finished' => nil)
         reset_session
-        target = new_user_session_path(:user => { :immediate => true })
+        target = new_user_session_path :user => { :immediate => true }
       end
 
-      Rails.logger.info "OID check: final state = #{load_oid_state.inspect}"
+      info "final state = #{load_oid_state.inspect}"
+      info "target = #{target.inspect}"
       redirect_to target unless target.blank?
+    end
+
+    private
+
+    def info(s)
+      Rails.logger.info "OID check: #{s}"
     end
 
     def load_oid_state
@@ -37,13 +66,27 @@ module OpenidClient
     def save_oid_state(state)
       cookies.signed[OpenidClient::Config.client_state_key] = {
         :value => state.to_json,
-        :expires => 15.minutes.from_now
+        :expires => 15.seconds.from_now
+        #:expires => 15.minutes.from_now
       }
     end
 
     def get_timestamp
       t = cookies[OpenidClient::Config.server_timestamp_key]
       if t.blank? then 'x' else t end
+    end
+
+    def recheck_needed(timestamp, state)
+      state['finished'] != timestamp and 
+        not request.path =~ /^#{new_user_session_path}\??/
+    end
+
+    def target_hash
+      if request.request_method != 'GET'
+        params.merge({ :_method => request.request_method })
+      else
+        params
+      end
     end
   end
 end
